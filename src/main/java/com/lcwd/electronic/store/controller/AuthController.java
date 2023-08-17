c3f3e3d6328cafe0dd3322afda02cd12ae4aa505
ankitnaitam <ankitnaitam2015@gmail.com>
2023-08-17 12:52:46 +0530

    Implemented JwtAuthenticationEntryPoint class for user authentication

--- src/main/java/com/lcwd/electronic/store/controller/AuthController.java
+++ src/main/java/com/lcwd/electronic/store/controller/AuthController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -27,4 +25,5 @@ public class AuthController {
         String name = principal.getName();
         return new ResponseEntity<>(mapper.map(userDetailsService.loadUserByUsername(name), UserDto.class), HttpStatus.OK);
     }
+    
 }
